44d0866b02ac7e250ed2cf9f8da45cd47cc024e6
wcrd <11898748+wcrd@users.noreply.github.com>
2019-11-23 10:00:17 +1100

    Add linting to dev and associated config files

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "plugins": [ "react" ],
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parser": "babel-eslint"
+}
