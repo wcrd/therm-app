a4e649bc2aa0bc4be2fb96e011009bf034a67221
wcrd <11898748+wcrd@users.noreply.github.com>
2020-08-29 09:59:36 +1000

    - Add new webpack dev server config - Update package.json to reference this config for dev server - Remove express from dependencies

---
+++ webpack.dev.js
@@ -0,0 +1,37 @@
+const path = require("path")
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  entry: {
+    app: './src/app.js',
+    about: './src/about.js',
+    theory: './src/theory.js'
+  },
+  devServer: {
+    port: 8080
+  },
+  module: {
+    rules: [
+      { test: /\.html$/, use: "html-loader" },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/html/index.html",
+      filename: "./index.html",
+      chunks: [ 'app' ],
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/html/about.html",
+      filename: "./about.html",
+      chunks: ['about']
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/html/theory.html",
+      filename: "./theory.html",
+      chunks: ['theory']
+    })
+  ]
+}
